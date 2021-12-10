06acbea8ae4ddf44ccf97143ee81844f420bf477
David Leach <davidleach724@gmail.com>
2021-12-10 13:34:49 -0700

    Create issues component

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,7 @@ export const fetchData = () => {
   return fetch("https://api.github.com/search/issues?q=repo:Anarios/return-youtube-dislike+type:issue")
   .then(response => response.json())
 }
+
+// https://api.github.com/search/issues?q=repo:Anarios/return-youtube-dislike+type:issue
+
+// 
---
+++ src/features/issues/issuesSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  value = []
+}
+
+const issuesSlice = createSlice({
+  name: 'issues',
+  initialState,
+  reducers: {
+    
+  }
+})
