9fb07e1ea39b93af8cf5180cfa606427767f4b83
David Leach <davidleach724@gmail.com>
2021-12-10 09:30:28 -0700

    Create incrementor

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
+import { Counter } from '../features/counter/Counter';
 import './App.css';
 
 function App() {
   return (
+    <>
     <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
+    <Counter />
+    </>
   );
 }
 
