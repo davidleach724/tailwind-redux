7f699d00f1036bcb219123afb4cf8748b6077cc0
David Leach <davidleach724@gmail.com>
2021-12-11 10:04:08 -0700

    Create buttons to reflect status

---
+++ src/features/filterStatus/FilterStatus.js
@@ -0,0 +1,33 @@
+import { useDispatch, useSelector } from "react-redux"
+import { displayAll, displayClosed, displayOpen } from "./filterStatusSlice"
+
+export const FilterStatus = () => {
+  const status = useSelector((state) => state.status.value)
+  const dispatch = useDispatch()
+
+  return (
+    <div>
+      <div>
+      <button
+          aria-label="Show Open"
+          onClick={() => dispatch(displayOpen())}
+        >
+          Show Open
+        </button>
+        <button
+          aria-label="Show Closed"
+          onClick={() => dispatch(displayClosed())}
+        >
+          Show Closed
+        </button>
+        <button
+          aria-label="Show All"
+          onClick={() => dispatch(displayAll())}
+        >
+          Show All
+        </button>
+        <br>{status}</br>
+      </div>
+    </div>
+  )
+}
